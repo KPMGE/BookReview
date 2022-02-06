b9cc69b7bf411381dd426a2b9e9a9cf20ded0647
KPMGE <jesuskevin254@gmail.com>
2022-02-06 20:29:26 -0300

    implement CRUD

--- backend/src/repositories/implementations/MysqlBooksRepository.ts
+++ backend/src/repositories/implementations/MysqlBooksRepository.ts
@@ -2,12 +2,12 @@ import { Book } from "../../entities/Book";
 import { IBooksRepository } from "../IBooksRepository";
 import { createPool } from "mysql2/promise";
 
-// Create the connection pool. 
+// Create the connection pool.
 const pool = createPool({
   host: "localhost",
   user: "root",
   database: "books_review",
-  password: '1234',
+  password: "1234",
   waitForConnections: true,
   connectionLimit: 10,
   queueLimit: 0,
@@ -15,10 +15,30 @@ const pool = createPool({
 
 export class MysqlBooksRepository implements IBooksRepository {
   async save({ id, title, author, description }: Book): Promise<void> {
-    console.log("TEST: ", id, title, author, description);
     await pool.query(
       "INSERT INTO books (id, title, author, description) VALUES(?,?,?,?);",
       [id, title, author, description]
+    );
+  }
+
+  async getAllBooks(): Promise<Book[]> {
+    const [rows] = await pool.query("SELECT * FROM books;");
+    return rows as any;
+  }
+
+  async delete(id: string): Promise<void> {
+    await pool.query("DELETE FROM books WHERE id=?", [id]);
+  }
+
+  async getBookById(id: string): Promise<Book> {
+    const [rows] = await pool.query("SELECT * FROM books WHERE id = ?;", [id]);
+    return rows as any;
+  }
+
+  async update(id: string, { author, description, title }: Book): Promise<void> {
+    await pool.query(
+      "UPDATE books SET title=?, author=?, description=? WHERE id = ?",
+      [title, author, description, id]
     );
   }
 }
--- backend/src/routes.ts
+++ backend/src/routes.ts
@@ -1,10 +1,25 @@
 import { Request, Response, Router } from "express";
 import { createBookController } from "./useCases/CreateBook";
+import { deleteBookController } from "./useCases/DeleteBook";
+import { getAllBooksController } from "./useCases/GetBooks";
+import { updateBookController } from './useCases/UpdateBook'
 
 const routes = Router();
 
+routes.get("/books", (request: Request, response: Response) => {
+  getAllBooksController.handle(request, response);
+});
+
 routes.post("/books/new", (request: Request, response: Response) => {
   createBookController.handle(request, response);
+});
+
+routes.delete("/books/delete/:id", (request: Request, response: Response) => {
+  deleteBookController.handle(request, response);
+});
+
+routes.put('/books/update/:id', (request: Request, response: Response) => {
+  updateBookController.handle(request, response);
 });
 
 export { routes };
